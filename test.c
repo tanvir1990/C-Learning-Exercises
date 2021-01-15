66f75c8393d82a009a6769c69fd132324923bdef
tanvir1990 <pavel.mhp@gmail.com>
2021-01-15 17:27:35 -0500

    Basic Syntax refreshing

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Basic Syntax Refreshing
+int main(){
+    int m = 10;
+    int n, o;
+    int* z;
+    z = &m;
+
+    printf ("z stores the address of m %x \n", &m);
+    //printf ("*z stores the address of m %u", *z);
+    printf ("*z stores the address of m %x", *(&z));
+
+    return 0;
+}
