348b7d49181549b610c9d9c427b816de9a2ed02c
tanvir1990 <pavel.mhp@gmail.com>
2021-01-18 14:24:16 -0500

    find the maximum using pointer

--- test.c
+++ test.c
@@ -2,21 +2,23 @@
 
 //Basic Syntax Refreshing
 
-int add(int *pa, int *pb){
-    int sum = *pa + *pb;
-    return sum;
-}
+// int add(int *pa, int *pb){
+//     int sum = *pa + *pb;
+//     return sum;
+// }
 int main(){
-    int a = 55;
-    int b = 1;
+    int a = 5;
+    int b = 10;
 
     int *pa, *pb;
     pa = &a;
     pb = &b;
 
-    int sum = add(pa, pb);
-
-    printf("%u", sum);
+    if ( *pa > *pb){
+        printf("%u is greater", *pa);
+    }
+    else 
+        printf("%u is greater", *pb);
     
 
     return 0;
