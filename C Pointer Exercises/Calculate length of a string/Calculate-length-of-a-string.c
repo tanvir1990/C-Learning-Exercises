9a32b480c00a40b6f4afcbfa31daa8e1c57f75a9
tanvir1990 <pavel.mhp@gmail.com>
2021-01-23 20:38:17 -0500

    used pointer to string and pointer to counter

--- C Pointer Exercises/Calculate length of a string/Calculate-length-of-a-string.c
+++ C Pointer Exercises/Calculate length of a string/Calculate-length-of-a-string.c
@@ -2,7 +2,17 @@
 
 //Basic Syntax Refreshing
 int main(){
-    char str[] = "Test Message";
+    char str[] = "Test";
+    int count = 0;
+    int * p_count = &count;
+    int i = 0;
+
+    while ( *(str + i) != '\0'){
+        *p_count = *p_count + 1;
+        i++;
+
+    }
+    printf("Length is %d", count);
     
     return 0;
 }
